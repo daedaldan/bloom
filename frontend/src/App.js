8037b2bb6a2140c25b8fe7e982fcab901ea45764
Daniel Wang <daniel22373@gmail.com>
2020-04-16 19:57:52 -0400

    Work on JWT authentication in React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 const users = [
@@ -58,25 +57,10 @@ const matches = [
   }
 ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <h1>Hello</h1>
+  }
 }
 
 export default App;
---
+++ frontend/src/components/Authentication/login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import { isEmail } from "validator";
+
+const required = value => {
+  if (!value) {
+    return (
+        <div role="alert">
+          This field is required.
+        </div>
+    );
+  }
+};
+
+const email = value => {
+  if (!isEmail(value)) {
+    return (
+        <div role="alert">
+          This is not a valid email.
+        </div>
+    );
+  }
+};
